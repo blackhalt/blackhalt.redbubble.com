d2f8cba34974a04e0689e25755769d55df2e40b4
BlackHalt <blackhalt@gmail.com>
2020-01-22 10:21:25 +0200

    Update Resize.sh

--- ImageMagick/Resize.sh
+++ ImageMagick/Resize.sh
@@ -3,3 +3,5 @@
 convert t.png -filter Lanczos -distort Resize 50x50 best.png
 convert -thumbnail 50 t.png 50.png
 
+# resize to 1000px:
+convert 1.png -filter Lanczos -distort Resize 1000x1000 2.png
