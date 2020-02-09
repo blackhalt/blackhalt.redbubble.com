5e51cb07e7f34fda69e6532be793c9429009c33d
BlackHalt <blackhalt@gmail.com>
2020-02-09 16:07:28 +0200

    Update Resize.sh

--- ImageMagick/Resize.sh
+++ ImageMagick/Resize.sh
@@ -5,3 +5,6 @@ convert -thumbnail 50 t.png 50.png
 
 # resize to 1000px:
 convert 1.png -filter Lanczos -distort Resize 1000x1000 2.png
+
+# Crop png images from center, in directory :
+mogrify -gravity center -crop 4096x4096+0+0 +repage *.png
